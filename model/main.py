9d6043fa589e0ea6b592c77c610b1b1ca42c679c
andreas armstrong <andreasarmstrong94@gmail.com>
2021-01-26 18:26:41 +0000

    initial commit of entry point

---
+++ model/main.py
@@ -0,0 +1,10 @@
+"""
+    File to be exectuted by Cloud Function.
+"""
+
+
+def main():
+    """
+        Entry point - Do not change name of this function.
+    """
+    return "Hello Fantasy Football"
