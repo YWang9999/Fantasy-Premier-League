494ff8379b38a8c3742375b88d2f47dc37a18686
andreas armstrong <andreasarmstrong94@gmail.com>
2021-01-27 22:09:28 +0000

    update with env var exports

--- model/startup_script.sh
+++ model/startup_script.sh
@@ -8,6 +8,10 @@ cd Fantasy-Premier-League/
 sudo apt install python3-pip
 pip3 install -r requirements.txt
 
+
+export PROJECT_ID=lbghack2021team14
+export PATH_TO_JSON_KEY=model/lbghack2021team14-7efa23028478.json
+
 # Run modelling
 python3 global_scraper.py
 python3 model/data_ingester.py
