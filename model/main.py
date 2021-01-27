c448255aabd9f5c205c5dd0e0195c6bc46363170
andreas armstrong <andreasarmstrong94@gmail.com>
2021-01-27 10:09:47 +0000

    check response

--- model/main.py
+++ model/main.py
@@ -7,4 +7,21 @@ def main(request):
     """
         Entry point - Do not change name of this function.
     """
-    return ("Hello from Jonny, Yi and Andreas")
+    # return ("Hello from Jonny, Yi and Andreas")
+    request_json = request.get_json(silent=True)
+    request_args = request.args
+
+    # if request_json and 'name' in request_json:
+    #     name = request_json['name']
+    # elif request_args and 'name' in request_args:
+    #     name = request_args['name']
+    # else:
+    #     name = 'World'
+    # return 'Hello {}!'.format(escape(name))
+    print(request_json)
+    print(type(request_json))
+    
+    print(request_args)
+    print(type(request_args))
+
+    return (request_json)
