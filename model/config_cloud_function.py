328e19718418bb6cda3c0923207b5d66cd8b3588
andreas armstrong <andreasarmstrong94@gmail.com>
2021-01-28 15:11:44 +0000

    bespoke config cloud function

---
+++ model/config_cloud_function.py
@@ -0,0 +1,25 @@
+
+from google.oauth2 import service_account
+import os
+import pandas_gbq
+from google.cloud import secretmanager
+
+
+WEBSCRAPE_DATA_PATH = os.path.abspath(os.path.join(os.getcwd(), 'data')) 
+
+INGESTED_DATA = "gw_raw"
+FEATURE_DATA = "features_df"
+PREDICTIONS = "predictions"
+
+secrets_client = secretmanager.SecretManagerServiceClient()
+request = {"name": f"projects/{PROJECT_ID}/secrets/service-account-key-compute-engine-user2/versions/latest"}
+response = secrets_client.access_secret_version(request)
+secret_string = response.payload.data.decode("UTF-8")
+
+# Update the in-memory credentials cache (added in pandas-gbq 0.7.0).
+pandas_gbq.context.credentials = secret_string
+
+PROJECT_ID = os.environ.get("PROJECT_ID")
+pandas_gbq.context.project = PROJECT_ID
+
+          
