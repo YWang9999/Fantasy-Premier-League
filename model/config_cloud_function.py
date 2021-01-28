a6d8548257e4ace547b7965bbab41369802bd064
andreas armstrong <andreasarmstrong94@gmail.com>
2021-01-28 15:42:48 +0000

    runtime env variables not working in cloud function for some god damn reason

--- model/config_cloud_function.py
+++ model/config_cloud_function.py
@@ -10,7 +10,9 @@ WEBSCRAPE_DATA_PATH = os.path.abspath(os.path.join(os.getcwd(), 'data'))
 INGESTED_DATA = "gw_raw"
 FEATURE_DATA = "features_df"
 PREDICTIONS = "predictions"
-PROJECT_ID = os.environ.get("PROJECT_ID")
+# PROJECT_ID = os.environ.get("PROJECT_ID")
+
+PROJECT_ID = 'lbghack2021team14'
 
 secrets_client = secretmanager.SecretManagerServiceClient()
 request = {"name": f"projects/{PROJECT_ID}/secrets/service-account-key-compute-engine-user2/versions/latest"}
