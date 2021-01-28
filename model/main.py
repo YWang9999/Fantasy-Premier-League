0ab7b07fce96e365496638cebc6b5a6cb8656232
andreas armstrong <andreasarmstrong94@gmail.com>
2021-01-28 14:14:45 +0000

    cloud function initialise

--- model/main.py
+++ model/main.py
@@ -2,6 +2,7 @@
     File to be exectuted by Cloud Function.
 """
 from flask import Flask, request, jsonify
+from optimised_selector import optimumTeam, best_transfer
 
 def main(request):
     """
@@ -10,5 +11,18 @@ def main(request):
     """
     data = request.get_json(force=True)
     print("Data sent is ", data)
-   
-    return jsonify(data['existing_team'] , data['free_transfers'])
+
+    data_existing_team = data['existing_team']
+    data_free_transfers = data['free_transfers']
+    data_budget = data['budget']
+
+
+    results = optimumTeam(
+        budget = data_budget,
+        number_of_players=None,
+        full_squad = True
+    )
+    results_json = results.to_json()
+    print(results_json)   
+    # return jsonify(data['existing_team'] , data['free_transfers'])
+    return results_json
