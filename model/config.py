92e3724be856ba343c9265663992307665c18823
YWang9999 <yiyi3278@gmail.com>
2021-01-29 13:46:14 +0000

    set up being able to choose not to score latest gw

--- model/config.py
+++ model/config.py
@@ -18,6 +18,7 @@ PREDICTIONS = "predictions.csv"
 PAST_WEEKS_NUM = [1, 3, 6, 12]
 TARGET_TYPE = "AVG"
 TARGET_WEEKS_INTO_FUTURE = 6
+WEEKS_TO_TRUNCATE = 0
 TIME_RELATED_FEATURES = ['assists', 'bonus', 'bps', 'clean_sheets', 'creativity',
                          'goals_conceded', 'goals_scored', 'ict_index', 'influence',
                          'minutes', 'own_goals', 'penalties_missed', 'penalties_saved', 'red_cards', 'saves',
--- model/modeller.py
+++ model/modeller.py
@@ -1,9 +1,16 @@
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
 from sklearn import ensemble
-from config import RAW_DATA_PATH, FEATURE_DATA, PREDICTIONS, TARGET_WEEKS_INTO_FUTURE, PARAMS
+from config import RAW_DATA_PATH, FEATURE_DATA, PREDICTIONS, TARGET_WEEKS_INTO_FUTURE, PARAMS, WEEKS_TO_TRUNCATE
 import os
 
+
+def truncate_previous_weeks(overall_data, weeks_to_truncate):
+    all_data = overall_data.reset_index(['career_gw'])
+    weeks_to_be_dropped = all_data.groupby(level=0).tail(weeks_to_truncate)
+    weeks_to_be_dropped.set_index('career_gw', append=True, inplace=True)
+    truncated_data = overall_data.drop(weeks_to_be_dropped.index)
+    return truncated_data
 
 def main():
     features = pd.read_csv(os.path.join(RAW_DATA_PATH, FEATURE_DATA),
@@ -18,13 +25,21 @@ def main():
         players_in_pos = features[features['element_type'] == pos]
 
         # Train-test split
+        if WEEKS_TO_TRUNCATE > 0:
+            truncated_data = truncate_previous_weeks(overall_data=players_in_pos,
+                                                     weeks_to_truncate=WEEKS_TO_TRUNCATE)
+        else:
+            truncated_data = players_in_pos.copy()
 
-        grouped = players_in_pos.reset_index(['career_gw'])
-        test = grouped.groupby(level=0).tail(min(1, TARGET_WEEKS_INTO_FUTURE))
-        test.set_index('career_gw', append=True, inplace=True)
+        grouped = truncated_data.reset_index(['career_gw'])
+
+        non_train = grouped.groupby(level=0).tail(min(1, TARGET_WEEKS_INTO_FUTURE))
+        non_train.set_index('career_gw', append=True, inplace=True)
         # test = players_in_pos.xs(players_in_pos.index.levels[1].max(), level=1, drop_level=False)
         # train = players_in_pos.drop(players_in_pos.index.levels[1].max(), level=1, axis=0)
-        train = players_in_pos.drop(test.index)
+        train = truncated_data.drop(non_train.index)
+        test = grouped.groupby(level=0).tail(1)
+        test.set_index('career_gw', append=True, inplace=True)
 
 
         # Drops rows with target NA which are not most recent month
